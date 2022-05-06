91e2283fe6961e7b149bcaaace457b494cbe0294
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2022-05-06 12:12:33 +0200

    Fix Dsp bug

--- findres/analyze.py
+++ findres/analyze.py
@@ -18,9 +18,11 @@ def correlate_waves(trace1, trace2, max_shift, **kwargs):
     elif trace1.std() == 0 or trace2.std() == 0:
         raise EmptyOrConstantTraceError("Normalized cross-correlation of constant traces is undefined.")
     if trace1.stats.npts >= trace2.stats.npts:
-        data1, data2, is_swapped = trace1.data, trace2.data, False
+        is_swapped = False
+        data1, data2 = trace1.data, trace2.data
     else:
-        data1, data2, is_swapped = trace2.data, trace1.data, True
+        is_swapped = True
+        data1, data2 = trace2.data, trace1.data
     data1 = np.concatenate([np.zeros(max_shift), data1, np.zeros(max_shift)])
     cc = correlate_template(data1, data2, **kwargs)
     argmax = np.argmax(cc)
@@ -34,11 +36,10 @@ def find_slope(cross_spectrum, frequency_range, params):
     coherence = cross_spectrum.cohe.flatten()
     frequency = cross_spectrum.freq.flatten()
     phase = cross_spectrum.phase.flatten()
-    coherence_threshold = params['cs_coherence_threshold']
     lower_frequency_bound = frequency_range[0],
     upper_frequency_bound = frequency_range[1] + params['coherence_frequency_upper_bound_correction']
 
-    indices = longest_subsequence_of_trues((coherence > coherence_threshold) &
+    indices = longest_subsequence_of_trues((coherence > params['cs_coherence_threshold']) &
                                            (frequency > lower_frequency_bound) & (frequency < upper_frequency_bound))
     if indices:
         start, stop = indices
